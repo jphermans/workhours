dc1ebfb5a92dcc0180509c4ef1fe03065a5c7425
macfreaker <munthouder.merrie-0w@icloud.com>
2025-03-29 19:00:02 +0100

    Initial Commit

---
+++ Workhours/WorkhoursApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkhoursApp.swift
+//  Workhours
+//
+//  Created by Jean-Pierre Hermans on 29/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct WorkhoursApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
